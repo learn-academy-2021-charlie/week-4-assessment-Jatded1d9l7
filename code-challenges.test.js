bb9d2bcd4380b53618c37f4dc828253c31c111f8
Jared Thomas <85851208+Jatded1d9l7@users.noreply.github.com>
2021-08-20 12:29:05 -0400

    modifications

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,13 +12,6 @@
 
 
 
-
-
-
-
-
-
-
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 // // a) Create a test with an expect statement using the       variable provided.
 // // b) Create the function that makes the test pass.
@@ -39,27 +32,20 @@
 // console.log(fruits)
 
 
-// describe("rem_1st_n_shfl", () => {
-//     test("takes in an array and removes the first item and shuffles the remaining content", () => {
-//       expect(rem_1st_n_shfl(["purple", "blue", "green", "yellow", "pink"])).toEqual("hi")
-//     })
-//   })
+describe("rem_1st_n_shfl", () => {
+    test("takes in an array and removes the first item and shuffles the remaining content", () => {
+      expect(rem_1st_n_shfl(["purple", "blue", "green", "yellow", "pink"])).toEqual("hi")
+    })
+  })
 
-//   const rem_1st_n_shfl = [] => { 
-//       array.shift([]]}
-//         )}
+  const rem_1st_n_shfl = [] => { 
+      array.shift([]]}
+        )}
 
-// var colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-// var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
-
-
-
-
-
-
-
+var colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
 
 
@@ -68,12 +54,7 @@
 // // a) Create a test with expect statements for each of the variables provided.
 // // b) Create the function that makes the test pass.
 
-// var nums1 = [3, 56, 90, -8, 0, 23, 6]
-// // Expected output: [-8, 90]
-// var nums2 = [109, 5, 9, 67 8, 24]
-// // Expected output: [5, 109]
-
-//pseudo code
+// pseudo code
 // Function takes in array
 // function = [array of numbers]
 // minMax = [] =>
@@ -81,26 +62,27 @@
 // Option 1: use a for loop
 // Option 2: use .map() or use .filter
 
-// describe("minMax", () => {
-//   test("takes in an array and returns an array of the min and max in that order", () => {
-//     var nums1 = [3, 56, 90, -8, 0, 23, 6]
-//     var nums2 = [109, 5, 9, 67, 8, 24]
-//     expect(minMax(nums1)).toEqual(-8, 90)
-//   })
-// })
+
+var nums1 = [3, 56, 90, -8, 0, 23, 6]
+// Expected output: [-8, 90]
+var nums2 = [109, 5, 9, 67 8, 24]
+// Expected output: [5, 109]
+
+// 
+
+describe("minMax", () => {
+  test("takes in an array and returns an array of the min and max in that order", () => {
+    var nums1 = [3, 56, 90, -8, 0, 23, 6]
+    var nums2 = [109, 5, 9, 67, 8, 24]
+    expect(minMax(nums1)).toEqual(-8, 90)
+  })
+})
  
-// const minMax = (array) => {
-//   return array.sort((a, b) => a - b)
-// }
+const minMax = (array) => {
+  return array.sort((a, b) => a - b)
+}
 
-// console.log(minmax(nums1))
-
-
-
-
-
-
-
+console.log(minmax(nums1))
 
 
 
@@ -110,11 +92,6 @@
 // // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 // // a) Create a test with an expect statement using the variables provided.
 // b) Create the function that makes the test pass.
-
-
-var testArray1 = [3, 7, 10, 5, 4, 3, 3]
-var testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 //pseudo code
 // function = {[],[]}
@@ -126,6 +103,10 @@ var testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Option2: merge the two arrays together and then filter out the duplicates
 // ? What method can I use that merges two arrays?
 // ? once I have merged the two arrays what method can I use to filter out the duplicates? 
+
+var testArray1 = [3, 7, 10, 5, 4, 3, 3]
+var testArray2 = [7, 8, 2, 3, 1, 5, 4]
+// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 describe('singleArray',()=>{
   it('takes in two arrays and returns one array without any duplicates',()=>{
